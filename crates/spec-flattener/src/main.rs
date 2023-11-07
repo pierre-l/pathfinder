88d8919623f255ab1caebf7fe804d2e299b91a92
pierre-l <pierre.larger@gmail.com>
2023-11-08 00:54:37 +0100

    Spec flattener: extract a `flatten_section` function

--- crates/spec-flattener/src/main.rs
+++ crates/spec-flattener/src/main.rs
@@ -1,5 +1,4 @@
-use serde_json::Value;
-use std::collections::HashMap;
+use serde_json::{Map, Value};
 
 #[tokio::main]
 async fn main() {
@@ -19,86 +18,89 @@ async fn main() {
 
     let mut flattened_schemas = serde_json::Map::new();
 
-    {
-        let schemas = root
-            .pointer_mut("/components/schemas")
-            .unwrap()
-            .as_object_mut()
-            .unwrap();
-
-        println!("Schemas: {}", schemas.len());
-
-        // TODO ugly
-        let mut schemas_left = 999;
-        while schemas_left > 0 {
-            // TODO Collect the schemas that are flat
-            // TODO Ugly
-            let mut flat = vec![];
-            for (name, definition) in schemas.iter_mut() {
-                if !object_fields(definition)
-                    .into_iter()
-                    .any(|(key, value)| key == "$ref" && value.as_str().is_some())
-                {
-                    println!("Flat: {}", name);
-                    flat.push(name.clone());
-                }
-            }
-
-            for flat in flat {
-                let definition = schemas.remove(flat.as_str()).unwrap();
-                if let Some(_) = flattened_schemas.insert(flat, definition) {
-                    panic!("A schema was replaced")
-                }
-            }
-
-            // TODO Flatten all the refs.
-            for (name, definition) in schemas.iter_mut() {
-                object_fields(definition)
-                    .into_iter()
-                    .for_each(|(key, mut value)| {
-                        if key == "$ref" {
-                            println!("Ref found: {}", key);
-                            let reference = value.as_str().unwrap();
-
-                            if !reference.starts_with("#/components/schemas/") {
-                                panic!()
-                            }
-
-                            let name = reference
-                                .split("#/components/schemas/")
-                                .collect::<Vec<&str>>()[1];
-
-                            // TODO REMOVE
-                            println!("Name {}", name);
-
-                            if let Some(definition) = flattened_schemas.get(name) {
-                                let mut flattened_reference = serde_json::Map::new();
-                                if let Some(_) = flattened_reference
-                                    .insert(reference.to_string(), definition.clone())
-                                {
-                                    panic!("A schema was replaced")
-                                }
-                                *value = Value::Object(flattened_reference);
-                                // TODO dbg!(&value);
-                            }
-                        }
-                    })
-            }
-
-            if schemas_left == schemas.len() {
-                panic!("No replacement during the last pass.")
-            }
-            schemas_left = schemas.len();
-            dbg!(&schemas_left);
-        }
-        *schemas = flattened_schemas.clone();
-    }
+    flatten_section(
+        &mut root,
+        &mut flattened_schemas,
+        "#/components/schemas/",
+        "/components/schemas",
+    );
 
     std::fs::write(
         format!("reference/{}", file),
-        serde_json::to_string_pretty(&root).unwrap(),
+        serde_json::to_string_pretty(&flattened_schemas).unwrap(),
     )
     .unwrap();
+}
+
+fn flatten_section(
+    root: &mut Value,
+    flattened_schemas: &mut Map<String, Value>,
+    reference_prefix: &str,
+    pointer: &str,
+) {
+    let schemas = root.pointer_mut(pointer).unwrap().as_object_mut().unwrap();
+
+    println!("Schemas: {}", schemas.len());
+
+    // TODO ugly
+    let mut schemas_left = 999;
+    while schemas_left > 0 {
+        // TODO Collect the schemas that are flat
+        // TODO Ugly
+        let mut flat = vec![];
+        for (name, definition) in schemas.iter_mut() {
+            if !object_fields(definition)
+                .into_iter()
+                .any(|(key, value)| key == "$ref" && value.as_str().is_some())
+            {
+                println!("Flat: {}", name);
+                flat.push(name.clone());
+            }
+        }
+
+        for flat in flat {
+            let definition = schemas.remove(flat.as_str()).unwrap();
+            if let Some(_) = flattened_schemas.insert(flat, definition) {
+                panic!("A schema was replaced")
+            }
+        }
+
+        // TODO Flatten all the refs.
+        for (name, definition) in schemas.iter_mut() {
+            object_fields(definition)
+                .into_iter()
+                .for_each(|(key, value)| {
+                    if key == "$ref" {
+                        let reference = value.as_str().unwrap();
+                        if !reference.starts_with(reference_prefix) {
+                            panic!()
+                        }
+
+                        let name = reference.split(reference_prefix).collect::<Vec<&str>>()[1];
+
+                        // TODO REMOVE
+                        println!("Name {}", name);
+
+                        if let Some(definition) = flattened_schemas.get(name) {
+                            let mut flattened_reference = serde_json::Map::new();
+                            if let Some(_) = flattened_reference
+                                .insert(reference.to_string(), definition.clone())
+                            {
+                                panic!("A schema was replaced")
+                            }
+                            *value = Value::Object(flattened_reference);
+                            // TODO dbg!(&value);
+                        }
+                    }
+                })
+        }
+
+        if schemas_left == schemas.len() {
+            panic!("No replacement during the last pass.")
+        }
+        schemas_left = schemas.len();
+        dbg!(&schemas_left);
+    }
 }
 
 // TODO Doc
@@ -112,7 +114,7 @@ fn object_fields(value: &mut Value) -> Vec<(&str, &mut Value)> {
         Value::Object(obj) => obj
             .iter_mut()
             .map(|(key, value)| match value {
-                Value::Object(obj) => object_fields(value),
+                Value::Object(_) => object_fields(value),
                 Value::Array(array) => array.iter_mut().map(object_fields).flatten().collect(),
                 Value::Null | Value::Bool(_) | Value::Number(_) | Value::String(_) => {
                     vec![(key.as_str(), value)]
@@ -122,20 +124,3 @@ fn object_fields(value: &mut Value) -> Vec<(&str, &mut Value)> {
             .collect(),
     }
 }
-
-/* TODO Remove?
-mod flattened {
-    use serde_json::Value;
-
-    struct Flattened(Value);
-
-    impl Flattened {}
-
-    impl From<Value> for Flattened {
-        fn from(value: Value) -> Self {
-            Self::check_for_refs();
-            todo!()
-        }
-    }
-}
-*/
